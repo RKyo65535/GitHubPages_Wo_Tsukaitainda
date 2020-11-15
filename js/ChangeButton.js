17e171495f1ef8105059757b53fa70702bdab638
ygkn <2000ygkn0713@gmail.com>
2020-11-15 18:03:08 +0900

    画像URL取得を関数に

--- js/ChangeButton.js
+++ js/ChangeButton.js
@@ -1,12 +1,13 @@
 let imageNum = 0;
-const imageURL = "image/artwork/ariawase/Item";
+
+const getImageUrl = () => `image/artwork/ariawase/Item${imageNum}.png`
 
 document.querySelector("#forwordButton").addEventListener("click", function () {
   imageNum = (imageNum + 1) % 100;
-  document.getElementById("MasterImage").src = imageURL + imageNum + ".png";
+  document.getElementById("MasterImage").src = getImageUrl();
 });
 
 document.querySelector("#backButton").addEventListener("click", function () {
   imageNum = (imageNum + 99) % 100;
-  document.getElementById("MasterImage").src = imageURL + imageNum + ".png";
+  document.getElementById("MasterImage").src = getImageUrl();
 });
