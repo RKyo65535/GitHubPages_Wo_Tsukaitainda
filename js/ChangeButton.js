642144f6ce82d67696890e59e0d1bac460b3ec12
ygkn <2000ygkn0713@gmail.com>
2020-11-15 18:05:10 +0900

    画像の要素を定数化

--- js/ChangeButton.js
+++ js/ChangeButton.js
@@ -1,13 +1,14 @@
 let imageNum = 0;
+const masterImage = document.getElementById("MasterImage");
 
 const getImageUrl = () => `image/artwork/ariawase/Item${imageNum}.png`
 
 document.querySelector("#forwordButton").addEventListener("click", function () {
   imageNum = (imageNum + 1) % 100;
-  document.getElementById("MasterImage").src = getImageUrl();
+  masterImage.src = getImageUrl();
 });
 
 document.querySelector("#backButton").addEventListener("click", function () {
   imageNum = (imageNum + 99) % 100;
-  document.getElementById("MasterImage").src = getImageUrl();
+  masterImage.src = getImageUrl();
 });
