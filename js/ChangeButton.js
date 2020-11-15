c6c6a6c3832f4eab0c88b4cff4533acd231a388f
ygkn <2000ygkn0713@gmail.com>
2020-11-15 18:13:19 +0900

    画像の更新をいい感じに関数に分けた

--- js/ChangeButton.js
+++ js/ChangeButton.js
@@ -1,14 +1,28 @@
+const totalImageCount = 100;
 let imageNum = 0;
+
+const incrementImageNum = () => {
+  imageNum = (imageNum + 1) % 100;
+};
+
+const decrementImageNum = () => {
+  imageNum = (imageNum - 1 + totalImageCount) % 100;
+};
+
+const getImageUrl = () => `image/artwork/ariawase/Item${imageNum}.png`;
+
+const updateImageUrl = () => {
+  masterImage.src = getImageUrl();
+};
+
 const masterImage = document.querySelector("#MasterImage");
 
-const getImageUrl = () => `image/artwork/ariawase/Item${imageNum}.png`
-
 document.querySelector("#forwordButton").addEventListener("click", function () {
-  imageNum = (imageNum + 1) % 100;
-  masterImage.src = getImageUrl();
+  incrementImageNum();
+  updateImageUrl();
 });
 
 document.querySelector("#backButton").addEventListener("click", function () {
-  imageNum = (imageNum + 99) % 100;
-  masterImage.src = getImageUrl();
+  decrementImageNum();
+  updateImageUrl();
 });
